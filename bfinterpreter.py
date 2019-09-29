77f0137836c77af6337af8511e540a4a6dac09c3
leszekbulawa <leszekbu@gmail.com>
2019-09-29 21:17:53 +0200

    Loops added; debug mode added

--- bfinterpreter.py
+++ bfinterpreter.py
@@ -1,11 +1,12 @@
 import sys
+import os
 
 class Interpreter(object):
-    def __init__(self, file_path):
+    def __init__(self, file_path, DEBUG=False):
         self.tape = [0] * 30000
         self.data_pointer = 0
-        self.instr_pointer = 0
-        self.jump_table = []
+        self.program_pointer = 0
+        self.DEBUG = DEBUG
 
         with open(file_path) as file:
             self.program = file.read()
@@ -14,7 +15,7 @@ class Interpreter(object):
         self.data_pointer += 1
 
     def move_pointer_left(self):
-        self.data_pointer += 1
+        self.data_pointer -= 1
 
     def increment_cell(self):
         self.tape[self.data_pointer] += 1
@@ -28,14 +29,34 @@ class Interpreter(object):
     def in_cell(self):
         self.tape[self.data_pointer] = sys.stdin.read(1)
 
-    # def jump_if_zero(self):
+    def jump_forward(self):
+        """Jumps to instruction after ] if current cell == 0"""
+        if self.tape[self.data_pointer] == 0:
+            while self.program[self.program_pointer] != ']':
+                self.program_pointer += 1
 
+    def jump_backwards(self):
+        """Jumps to ["""
+        while self.program[self.program_pointer] != '[':
+            self.program_pointer -= 1
+        self.program_pointer -= 1
 
     def print_program(self):
         print(self.program)
 
     def run_step(self):
-        instr = self.program[self.instr_pointer]
+        try:
+            instr = self.program[self.program_pointer]
+        except IndexError:
+            if self.DEBUG:
+                print('tape[0:100]:')
+                print(self.tape[:100])
+                print('End of program')
+            exit(0)
+        if self.DEBUG:
+            _ = input('Press ENTER for next step')
+            print('Cell number: {}, cell value: {}'.format(self.data_pointer, self.tape[self.data_pointer]))
+            print('Instruciton: {}, instruction ptr: {}'.format(self.program[self.program_pointer], self.program_pointer))
         if instr == '>':
             self.move_pointer_right()
         if instr == '<':
@@ -48,11 +69,17 @@ class Interpreter(object):
             self.out_cell()
         if instr == ',':
             self.in_cell()
-        self.instr_pointer += 1
+        if instr == '[':
+            self.jump_forward()
+        if instr == ']':
+            self.jump_backwards()
+        self.program_pointer += 1
     
     def run(self):
-        for _ in range(len(self.program)):
+        while True:
             self.run_step()
 
-interpreter = Interpreter('test.bf')
+
+DEBUG = os.environ.get('DEBUG', 0)
+interpreter = Interpreter(sys.argv[1], DEBUG)
 interpreter.run()
